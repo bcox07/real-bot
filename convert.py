e3af109654e3d6b4bb353f838aa5411762017c32
bcox07 <brian.cox07@gmail.com>
2024-11-23 16:34:58 -0700

    Update convert program

--- convert.py
+++ convert.py
@@ -1,9 +1,20 @@
-import subprocess
 from moviepy import VideoFileClip
+import os
 
-fileLocation = './clips/anubis-a-molly.mp4'
-gifLocation = './clips/anubis-a-molly.gif'
+from pathlib import Path
 
-videoClip = VideoFileClip(fileLocation).resized(0.4)
+parentDirectoryString = './clips/ancient'
 
-videoClip.write_gif(gifLocation, fps=15)
+directory = os.fsencode(parentDirectoryString)
+    
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    filenameWithoutExtension = Path(filename).stem
+    
+    if filename.endswith(".mp4"): 
+        fileLocation = os.path.join(parentDirectoryString, filename)
+        gifLocation = os.path.join(parentDirectoryString, f'{filenameWithoutExtension}.gif')
+
+        videoClip = VideoFileClip(fileLocation).resized(0.4)
+        videoClip.write_gif(gifLocation, fps=15)
+        continue
